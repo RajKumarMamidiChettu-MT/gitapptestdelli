f5f96ab066902f5c32d8560a69c5c2c8c89be5ec
RajKumarMamidiChettu-MT <84956498+RajKumarMamidiChettu-MT@users.noreply.github.com>
2021-10-07 16:33:40 +0530

    changed app js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 app.get('/', (req,res) => res.send(process.env));
 app.listen(port, () => console.log('server is running on port'+port ));
